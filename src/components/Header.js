5d0042c1c4e27cd5c4dc931807e9d6336d317838
sam jacob <sam218708@gmail.com>
2019-01-07 17:19:57 +0400

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
+import PropTypes from "prop-types";
+
 function Header(props) {
   const { branding } = props;
   return (
     <div>
-      <h1>{props.branding}</h1>
+      <h1>{branding}</h1>
     </div>
   );
 }
+
+Header.defaultProps = {
+  branding: "Contact Manager"
+};
+
+Header.proptypes = {
+  branding: PropTypes.object.isRequired
+};
 
 export default Header;
